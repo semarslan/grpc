bd2eaf54329f6f8a5123a5476fc904a91efff986
semarslan <94.sema.arslan@gmail.com>
2021-11-02 11:49:41 +0300

    Initial commit

---
+++ src/main/java/com/semarslan/grpcservice/GrpcserviceApplication.java
@@ -0,0 +1,13 @@
+package com.semarslan.grpcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semarslan/grpcservice/GrpcserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semarslan.grpcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
